882ba433d4327a495b1bca72a3a834a45956f27f
sachchapratapsingh <sachchapratapsingh@gmail.com>
2021-07-26 22:27:37 +0530

    Details Page Progress

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Home from './home/Home';
-
+import { Switch,Link, Route } from "react-router-dom";
+import BookShow from './bookshow/BookShow';
+import Details from './details/Details';
 
 export default class Controller extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+    }
    render() {
       return (
-         <div>
-            <Home/>
+         <div>       
+             <div>    
+             
+            <Link to="/home"></Link>
+                </div>
+               <Switch>
+                  <div>
+                     <Route path="/home" component={Home}></Route>
             
+                     <Route exact path="/movie/BookShow"  component={BookShow}
+></Route>
+                     <Route exact path="/movie/:id" render={(props)=>{return(
+    <Details id={props.match.params.id}/>)
+}}></Route>
+                        </div>
+                        </Switch>
          </div>
       );
    }
